2e1e0c69929ba2bc3b23cc1a98ba72d1a907e4a3
anirban_cpp <anirbanbanerjee844@gmail.com>
2023-11-24 10:50:24 +0530

    to-do-list done

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container')
+
+function addTask() {
+    if (inputBox.value === "")
+        alert("You need to provide a todo")
+    else {
+        let li = document.createElement('li')
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span)
+    }
+    inputBox.value = ""
+    saveData()
+}
+
+listContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+        console.log("event", e.target.classList)
+        e.target.classList.toggle("checked")
+        saveData()
+    }
+    else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove()
+        saveData()
+    }
+}, false)
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
